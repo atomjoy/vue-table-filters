6b5e52902c5f32a3daa39b06aa22ec6898e3a736
Atomjoy <mluk123@proton.me>
2026-09-07 15:49:53 +0200

    Add files via upload

--- controllers/UserFacets.php
+++ controllers/UserFacets.php
@@ -8,14 +8,12 @@ use Spatie\Permission\Models\Role;
 
 trait UserFacets
 {
-    public function facetsWithSearch(Request $request, $guard = 'web')
+    public function facets(Request $request, $guard = 'web')
     {
         $search = $request->input('search');
         $selectedRoles = $request->validated()['roles'];
         // $selectedRoles = $request->input('roles', []);
-        // $guardName = (new User())->getDefaultGuardName();
 
-        // Podzapytanie wyszukiwarki
         $subQuery = User::select('id');
         if (!empty($search)) {
             $subQuery->where(fn($q) => $q->where('users.name', 'like', "%{$search}%")
@@ -23,14 +21,53 @@ trait UserFacets
         }
 
         return Role::query()
+            ->where('roles.guard_name', $guard)
             // Zliczamy użytkowników przypisanych do roli, ale tylko z grupy wyszukanej
             ->withCount(['users' => fn($q) => $q->whereIn('users.id', $subQuery)])
-            ->where('roles.guard_name', $guard)
             // Filtrujemy same modele ról (w tabeli roles) po ich nazwie
             ->when(!empty($selectedRoles), fn($query) => $query->whereIn('roles.name', $selectedRoles))
-            ->get(['id', 'name', 'guard_name'])->keyBy('name')->map(fn($role) => [
+            ->get(['id', 'name'])
+            ->keyBy('name')->map(fn($role) => [
                 'id'    => $role->id,
                 'count' => $role->users_count
+            ]);
+    }
+
+    public function facetsWithSearch(Request $request, $guard = 'web')
+    {
+        $search = $request->input('search');
+        $selectedRoles = $request->validated()['roles'];
+        // $selectedRoles = $request->input('roles', []);
+
+        $subQuery = User::select('id');
+        if (!empty($search)) {
+            $subQuery->where(fn($q) => $q->where('users.name', 'like', "%{$search}%")
+                ->orWhere('users.email', 'like', "%{$search}%"));
+        }
+
+        return Role::query()
+            ->withCount(['users' => fn($q) => $q->whereIn('users.id', $subQuery)])
+            ->where('roles.guard_name', $guard)
+            ->when(!empty($selectedRoles), fn($query) => $query->whereIn('roles.name', $selectedRoles))
+            ->get(['id', 'name'])->map(fn($role) => [
+                'value' => $role->name,
+                'label' => ucfirst($role->name),
+                'count' => $role->users_count,
+            ]);
+    }
+
+    public function facetsWithBuilder(Builder $queryBeforePaginate, $guard = 'web')
+    {
+        $subQuery = clone($queryBeforePaginate ?? \App\Models\User::query());
+
+        return Role::query()
+            ->withCount(['users' => fn($q) => $q->whereIn('users.id', $subQuery->select('users.id'))])
+            ->where('roles.guard_name', $guard)
+            ->get(['roles.id', 'roles.name'])
+            ->map(fn($role) => [
+                'value' => $role->name,
+                'label' => ucfirst($role->name),
+                'count' => $role->users_count,
             ]);
     }
 }
